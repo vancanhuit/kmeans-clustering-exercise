2cbe2f5c47f7c67de38815a1818273f4ae62d073
vancanhuit <vancanhuit@gmail.com>
2017-12-02 11:44:08 +0700

    Use K-means in scikit-learn

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import helpers
+from sklearn.cluster import KMeans
 
 num_clusters, original_labels, points = helpers.generate_data_points()
 
@@ -10,3 +11,11 @@ print(centers)
 print('Total iterations: {}'.format(it))
 
 helpers.display(points, labels)
+
+kmeans = KMeans(n_clusters=3, random_state=0).fit(points)
+print('Centers found by scikit-learn library: ')
+print(kmeans.cluster_centers_)
+pred_label = kmeans.predict(points)
+print('Total iterations using scikit-learn library: {}'.format(kmeans.n_iter_))
+
+helpers.display(points, pred_label)
